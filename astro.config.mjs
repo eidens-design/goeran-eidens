962c0c609e508ba62911e83605d23965e4485a5a
eidens-design <eidens@posteo.me>
2023-02-18 14:29:51 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
+
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://goeran-eidens.de',
+  integrations: [sitemap(),astroI18next()]
+});
